3508d02c41cf8ada435d18a475a4e1eecf076a41
“nicolassistema” <nicolas.letticugna@gmail.com>
2023-05-20 16:27:34 -0300

    “Subiendo_entregable_Cliente_API_Rest”

---
+++ ClientApiRestLetticugna/src/main/java/edu/coderhouse/ClientApiRestLetticugna/ClientApiRestLetticugnaApplication.java
@@ -0,0 +1,13 @@
+package edu.coderhouse.ClientApiRestLetticugna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApiRestLetticugnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApiRestLetticugnaApplication.class, args);
+	}
+
+}
---
+++ ClientApiRestLetticugna/src/test/java/edu/coderhouse/ClientApiRestLetticugna/ClientApiRestLetticugnaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.coderhouse.ClientApiRestLetticugna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApiRestLetticugnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
