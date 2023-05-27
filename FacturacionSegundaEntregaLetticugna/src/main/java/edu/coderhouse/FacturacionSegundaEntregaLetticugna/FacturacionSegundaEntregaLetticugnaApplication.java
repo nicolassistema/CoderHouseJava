e1bb50a71cda74cfacc98ee3663e08f8fe6ce41b
“nicolassistema” <nicolas.letticugna@gmail.com>
2023-05-27 00:09:18 -0300

    Subiendo segunda entrega facturacion

---
+++ FacturacionSegundaEntregaLetticugna/src/main/java/edu/coderhouse/FacturacionSegundaEntregaLetticugna/FacturacionSegundaEntregaLetticugnaApplication.java
@@ -0,0 +1,13 @@
+package edu.coderhouse.FacturacionSegundaEntregaLetticugna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionSegundaEntregaLetticugnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionSegundaEntregaLetticugnaApplication.class, args);
+	}
+
+}
---
+++ FacturacionSegundaEntregaLetticugna/src/test/java/edu/coderhouse/FacturacionSegundaEntregaLetticugna/FacturacionSegundaEntregaLetticugnaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.coderhouse.FacturacionSegundaEntregaLetticugna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionSegundaEntregaLetticugnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
