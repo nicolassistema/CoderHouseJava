5a1cd5bb4a5927628f0b08129af9204cf6165cbe
“nicolassistema” <nicolas.letticugna@gmail.com>
2023-06-17 20:35:21 -0300

    suebiendo Entrega Final

---
+++ FacturacionEntregaFinalLetticugna/src/main/java/com/coderHouse/FacturacionEntregaFinalLetticugna/FacturacionEntregaFinalLetticugnaApplication.java
@@ -0,0 +1,13 @@
+package com.coderHouse.FacturacionEntregaFinalLetticugna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionEntregaFinalLetticugnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionEntregaFinalLetticugnaApplication.class, args);
+	}
+
+}
---
+++ FacturacionEntregaFinalLetticugna/src/test/java/com/coderHouse/FacturacionEntregaFinalLetticugna/FacturacionEntregaFinalLetticugnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderHouse.FacturacionEntregaFinalLetticugna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionEntregaFinalLetticugnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
