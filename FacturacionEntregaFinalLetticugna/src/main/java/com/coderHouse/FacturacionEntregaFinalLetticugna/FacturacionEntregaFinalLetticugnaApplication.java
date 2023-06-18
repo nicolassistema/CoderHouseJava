f0d0cb17e98cee654f7bdac81d5aa8245773db87
“nicolassistema” <nicolas.letticugna@gmail.com>
2023-06-17 21:22:16 -0300

    suebiendo Entrega Final - archivos de arranque y bla bla

--- FacturacionEntregaFinalLetticugna/src/main/java/com/coderHouse/FacturacionEntregaFinalLetticugna/FacturacionEntregaFinalLetticugnaApplication.java
+++ FacturacionEntregaFinalLetticugna/src/main/java/com/coderHouse/FacturacionEntregaFinalLetticugna/FacturacionEntregaFinalLetticugnaApplication.java
@@ -1,13 +1,16 @@
 package com.coderHouse.FacturacionEntregaFinalLetticugna;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FacturacionEntregaFinalLetticugnaApplication {
+public class FacturacionEntregaFinalLetticugnaApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FacturacionEntregaFinalLetticugnaApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(FacturacionEntregaFinalLetticugnaApplication.class, args);}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Server listening. Access H2 on: http://localhost:8888/h2-console");
 	}
-
 }
